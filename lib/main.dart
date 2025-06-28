24018ac45ff5029571f7f9c5666f7dbcbcc3d4df
Mikiyas Kebede <mknewkebede@gmail.com>
2025-06-28 22:42:21 +0300

    commented dotenv load

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
